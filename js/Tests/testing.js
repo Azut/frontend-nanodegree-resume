5f17209c63fd71403d98d5c61234ba98a112307a
Fernando Ramos Miracle <framos.miracle@gmail.com>
2014-11-13 22:57:31 +0100

    Create first JS example with vars and replace() on file "testing.js". Also emptied resumeBuilder.js

---
+++ js/Tests/testing.js
@@ -0,0 +1,19 @@
+// $("#main").append("Fernando Ramos Miracle");
+
+// [string].replace([old],[new])
+
+var email = "framos.miracle@something.com";
+
+var newEmail = email.replace("someting", "gmail");
+
+console.log(email);
+console.log(newEmail);
+
+var awesomeThoughts = "I am Fernando, and I am AWESOME";
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
+
+$("#main").append(funThoughts);
