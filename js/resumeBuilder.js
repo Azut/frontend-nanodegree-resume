bf61d9960a76687e1bb0adfb04c56d40176355a3
Fernando Ramos Miracle <framos.miracle@gmail.com>
2014-11-13 23:14:23 +0100

    Add name and role prepends.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,8 @@
+var name = "Fernando Ramos Miracle";
+var role = "Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
